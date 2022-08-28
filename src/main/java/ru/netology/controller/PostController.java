b74b099dcfc4c332a6603d749eae2e07e94a87e7
ArtJDev <artemkurilenko1983@yandex.ru>
2022-08-28 20:54:07 +0300

    Commit 28.08.2022

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -9,43 +9,47 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class PostController {
-  public static final String APPLICATION_JSON = "application/json";
-  private final PostService service;
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
 
-  public PostController(PostService service) {
-    this.service = service;
-  }
+    public PostController(PostService service) {
+        this.service = service;
+    }
 
-  public void all(HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-  }
+    public void all(HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var data = service.all();
+        final var gson = new Gson();
+        response.getWriter().print(gson.toJson(data));
+    }
 
-  public void getById(long id, HttpServletResponse response) throws IOException {
-    // TODO: deserialize request & serialize response
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.getById(id);
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-  }
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        // TODO: deserialize request & serialize response
+        try {
+            response.setContentType(APPLICATION_JSON);
+            final var data = service.getById(id);
+            final var gson = new Gson();
+            response.getWriter().print(gson.toJson(data));
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
 
-  public void save(Reader body, HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
-    final var post = gson.fromJson(body, Post.class);
-    final var data = service.save(post);
-    response.getWriter().print(gson.toJson(data));
-  }
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var gson = new Gson();
+        final var post = gson.fromJson(body, Post.class);
+        final var data = service.save(post);
+        response.getWriter().print(gson.toJson(data));
+    }
 
-  public void removeById(long id, HttpServletResponse response) throws IOException {
-    // TODO: deserialize request & serialize response
-    service.removeById(id);
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-
-  }
+    public void removeById(long id, HttpServletResponse response) throws IOException {
+        // TODO: deserialize request & serialize response
+        try {
+            service.removeById(id);
+            response.setStatus(HttpServletResponse.SC_ACCEPTED);
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
 }
--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,20 +1,17 @@
 package ru.netology.repository;
 
 import ru.netology.model.Post;
+import ru.netology.exception.NotFoundException;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicLongArray;
-import java.util.stream.Stream;
 
-// Stub
 public class PostRepository {
-    private final AtomicLong newId = new AtomicLong();
-    private final Set<Long> idList = new CopyOnWriteArraySet<>();
+    private final AtomicLong newId = new AtomicLong();                 //хранит id по порядку
+    private final Set<Long> usedId = new CopyOnWriteArraySet<>();      //хранит список использованных id
+    private final Set<Long> freeId = new CopyOnWriteArraySet<>();      //хранит список свободных id
     private final List<Post> posts = new CopyOnWriteArrayList<>();
 
     public List<Post> all() {
@@ -22,7 +19,6 @@ public class PostRepository {
     }
 
     public Optional<Post> getById(long id) {
-        //добавить if на проверку номера id
         for (Post post : posts) {
             if (post.getId() == id) {
                 return Optional.of(post);
@@ -32,29 +28,67 @@ public class PostRepository {
     }
 
     public Post save(Post post) {
-        if (posts.size() == 0 && post.getId() == 0) {
-            post.setId(1);
-            posts.add(post);
-            newId.set(post.getId());
-            idList.add(newId.get());
+        if (newId.get() == 0) {         //сразу присваиваем newId = 1
+            newId.set(1);
         }
-        if (posts.size() == 0 && post.getId() != 0) {
-            posts.add(post);
-            newId.set(post.getId());
-            idList.add(newId.get());
-        }
-        if (posts.size() != 0 && post.getId() == 0) {
-//            long nextId = idList.stream().min(Comparator.naturalOrder()).get();
-//            while (!idList.contains(nextId)) {
-//                post.setId(nextId);
-//                posts.add(post);
-//                nextId++;
-//            }
+
+        if (posts.size() == 0) {            //если список постов пустой, то
+            if (post.getId() == 0) {            //если пришел пост с id = 0
+                post.setId(newId.get());            //присваиваем посту id = 1
+                posts.add(post);                    //и добавляем его в лист
+                usedId.add(newId.get());            //добавляем newId в список использованных id
+                newId.incrementAndGet();            //увеличиваем newId на 1
+            } else {                            //иначе, если id поста != 0, то
+                posts.add(post);                    //просто добавляем пост с этим id в список постов
+                usedId.add(post.getId());           //добавляем newId в список использованных id
+            }
+        } else {                            //иначе если список постов не пустой, то
+            if (post.getId() == 0) {            //если пришел пост с id = 0, то
+                if (!freeId.isEmpty()) {            //смотрим есть ли свободные номера для id, если да, то
+                    post.setId(freeId.stream().min(Comparator.naturalOrder()).get());   //присваиваем посту минимальный id из списка свободных
+                    freeId.remove(post.getId());        //удаляем номер id из списка свободных
+                    posts.add(post);                    //добавляем пост в список постов
+                    usedId.add(post.getId());           //добавляем номер id в список использованных
+                } else {                            //иначе, если свободных номеров для id нет, то
+                    while (usedId.contains(newId.get())) {  //в цикле смотрим, содержится ли следующий по порядку id в списке использованных
+                        newId.incrementAndGet();            //каждый раз увеличивая его на 1
+                    }                                       //как только находится следующий не использованный по порядку номер id, выходим из цикла
+                    post.setId(newId.get());            //присваиваем посту новый newId
+                    posts.add(post);                    //добавляем пост в список постов
+                    usedId.add(newId.get());            //добавляем номер нового id в список использованных
+                }
+            } else {                            //иначе, если пришел пост с id != 0, то
+                if (usedId.contains(post.getId())) {  //смотрим, есть ли уже такой id в списке использованных, если да, то
+                    for (Post post1 : posts) {           //ищем пост с таким же id
+                        if (post1.getId() == post.getId() && !post1.getContent().equals(post.getContent())) { //когда найдется пост с таким же id, сравниваем контент, если контент разный, то
+                            posts.remove(post1);         //удаляем старый пост из списка
+                            posts.add(post);             //добавляем новый пост в список
+                        }                                //иначе ничего не делаем
+                    }
+                } else {                              //иначе, если поста с таки id не существует, то
+                    posts.add(post);                     //добавляем пост в список
+                    usedId.add(post.getId());            //добавляем номер id поста в список использованных
+                }
+            }
         }
         return post;
     }
 
     public void removeById(long id) {
-
+        if (usedId.contains(id)) {          //если id содержится в списке использованных id, то
+            for (Post post1 : posts) {          //ищем пост с таким id в списке постов
+                if (post1.getId() == id) {          //если найден, то
+                    posts.remove(post1);                //удаляем пост из списка
+                    usedId.remove(id);                  //удаляем id из списка использованных
+                    freeId.add(id);                     //добавляем id в список свободных
+                }
+            }
+        } else {                            //иначе если поста с таким id нет в списке использованных
+            throw new NotFoundException();      //выбрасываем ошибку в контроллер
+        }
+        if (posts.isEmpty()) {              //если удалили все посты из списка
+            newId.set(1);                       //выставляем нумерацию на 1
+            freeId.clear();                     //очищаем список свободных id
+        }
     }
 }

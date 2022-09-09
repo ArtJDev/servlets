d78259d46c0ffbcbd4f1d304a371fdd14fc55221
ArtJDev <artemkurilenko1983@yandex.ru>
2022-09-09 09:49:39 +0300

    Commit 09.09.2022

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class PostRepository {
-    private final AtomicLong newId = new AtomicLong();                 //хранит id по порядку
+    private final AtomicLong newId = new AtomicLong();
     private final Map<Long, Post> posts = new ConcurrentHashMap<>();
 
     public List<Post> all() {
@@ -20,42 +20,24 @@ public class PostRepository {
     }
 
     public Post save(Post post) {
-        if (posts.isEmpty()) {              //если список постов пустой, то
-            if (post.getId() == 0) {            //если пришел пост с id = 0
-                post.setId(newId.incrementAndGet());   //присваиваем посту id = 1 (самый первый случай)
-                posts.put(post.getId(), post);          //и добавляем его в лист
-            } else {                            //иначе, если id поста != 0, то
-                posts.put(post.getId(), post);      //просто добавляем пост с этим id в список постов
-            }
-        } else {                            //иначе если список постов не пустой, то
-            if (post.getId() == 0) {            //если пришел пост с id = 0
-                newId.set(1);                              //устанавливаем id = 1
-                while (posts.containsKey(newId.get())) {   //и пробегаемся по ключам мапы до тех пор,
-                    newId.incrementAndGet();               //пока не будет подобран следующий по порядку id
-                }
-                post.setId(newId.get());                   //присваиваем посту новый id
-                posts.put(post.getId(), post);          //кладем пост в мапу
-            } else {                            //иначе, если пришел пост с id != 0, то
-                if (posts.containsKey(post.getId())) {  //смотрим, есть ли уже такой id в мапе, если да, то
-                    if (!posts.get(post.getId()).equals(post)) {    //если старый пост и новый пост отличаются
-                        posts.replace(post.getId(), post);              //заменяем старый пост новым
-                    }                                               //иначе ничего не делаем
-                } else {                                //иначе, если поста с таки id не существует, то
-                    posts.put(post.getId(), post);                  //добавляем пост в мапу
-                }
+        if (post.getId() == 0) {
+            post.setId(newId.incrementAndGet());
+            posts.put(post.getId(), post);
+        } else {
+            if (posts.containsKey(post.getId()) && !posts.get(post.getId()).equals(post)) {
+                posts.replace(post.getId(), post);
+            } else {
+                posts.put(post.getId(), post);
             }
         }
         return post;
     }
 
     public void removeById(long id) {
-        if (posts.containsKey(id)) {        //если id содержится в списке ключей мапы
-            posts.remove(id);                   //удаляем пост из мапы
-        } else {                            //иначе если поста с таким id нет в мапе
-            throw new NotFoundException();      //выбрасываем ошибку в контроллер
-        }
-        if (posts.isEmpty()) {              //если удалили все посты из списка
-            newId.set(0);                       //выставляем нумерацию на 0
+        if (posts.containsKey(id)) {
+            posts.remove(id);
+        } else {
+            throw new NotFoundException();
         }
     }
 }
